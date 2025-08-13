8d7699376a34e5810514e75525f977edd79fa602
Chris Scheppler <devorg@cloudnerd.com>
2025-08-13 03:56:35 +0000

    CI deployment from job `InsiderNYC-Prod`

--- unpackaged/main/default/lwc/dateFieldDayViewer/dateFieldDayViewer.js
+++ unpackaged/main/default/lwc/dateFieldDayViewer/dateFieldDayViewer.js
@@ -4,42 +4,35 @@ import getDateFields from '@salesforce/apex/DateFieldDayViewerController.getDate
 export default class DateFieldDayViewer extends LightningElement {
     @api recordId;
     @api objectApiName;
-    @api fieldSetName;
-
-    dateFields;
+    @api fieldSetName; // Deprecated - no longer used
+    dateFields = [];
     error;
-    title;
+    title = '';
 
-    @wire(getDateFields, { objectApiName: '$objectApiName', recordId: '$recordId', fieldSetName: '$fieldSetName' })
+    @wire(getDateFields, { objectApiName: '$objectApiName', recordId: '$recordId' })
     wiredFields({ error, data }) {
         if (data) {
-            const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-            console.log('Date fields data:', data);
-
-            this.dateFields = data
-                .filter(field => field.label !== 'RecordType')
-                .map(field => {
-                    const dateObj = new Date(field.value.replace(' ', 'T'));
-                    return {
-                        label: field.label,
-                        value: field.value.replace(' ', 'T'),
-                        day: weekday[dateObj.getDay()]
-                    };
-            });
-
-            let recordTypeField = data.find(field => field.label === 'RecordType');
-            let recordType = recordTypeField ? recordTypeField.value : null;
-            
-
-            this.title = recordType ? `${recordType} Days` : `${this.objectApiName} Days`;
-
-            console.log('Processed date fields:', this.dateFields);
+            this.dateFields = data.filter(field => field.label !== 'RecordType');
+            this.setTitle(data);
             this.error = undefined;
         } else if (error) {
             console.error('Error fetching date fields:', error);
-            this.error = 'Error loading date fields';
-            this.dateFields = undefined;
+            this.error = 'Unable to load date fields. Please try again.';
+            this.dateFields = [];
         }
     }
 
+    setTitle(data) {
+        const recordTypeField = data.find(field => field.label === 'RecordType');
+        const recordType = recordTypeField?.value;
+        this.title = recordType ? `${recordType} Days` : `${this.objectApiName} Days`;
+    }
+
+    get hasDateFields() {
+        return this.dateFields && this.dateFields.length > 0;
+    }
+
+    get hasError() {
+        return Boolean(this.error);
+    }
 }
